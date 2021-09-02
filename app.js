84d21523d5b8a31f30a71605d15dcacf027079d8
Lawrence J <lawrenceg563@gmail.com>
2021-09-02 15:53:15 +0530

    seperating code to routes and controllers folder to achieve mvc pattern

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+const postRoutes = require('./routes/posts');
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("hello world");
-});
+app.use(morgan("dev"));
+
+app.use("/", postRoutes);
 
 const port=8080;
 app.listen(8080, ()=>{console.log(`node app is listerning to ${port}`)});
