452d323936a0a12f9ad7ba5ab7fbbfda855c57fa
Lawrence J <lawrenceg563@gmail.com>
2021-09-02 17:03:26 +0530

    installed dotenv bodyparser mongoose created mongoose model to store data created controller method to store the post data to db

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const postRoutes = require('./routes/posts');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+dotenv.config();
 
 const app = express();
 
-app.use(morgan("dev"));
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true}).then(()=> console.log("DB Connected"));
+mongoose.connection.on("error",err=>{console.log(`DB connection error: ${err.message}`)});
 
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.use("/", postRoutes);
 
-const port=8080;
+const port= process.env.PORT||8080;
 app.listen(8080, ()=>{console.log(`node app is listerning to ${port}`)});
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,24 @@
-const getPosts = (req,res)=>{
-    res.send("hello world");
+const Post = require("../models/posts");
+
+const getPosts = (req, res) => {
+  res.json({ posts: [{ title: "first post" }, { title: "second post" }] });
+};
+
+const createPost =(req,res)=>{
+    const post = new Post(req.body);
+    console.log('creating post:',req.body);
+    post.save((err,result)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        res.status(200).json({
+            post:result
+        })
+    })
 }
 
-module.exports ={
-    getPosts
-}
+module.exports = {
+  getPosts,createPost
+};
